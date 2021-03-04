f5f3e4604c766eaa61d2f4500f0bbfe1e1388030
mebilychen <1013712127@qq.com>
2021-03-04 20:03:15 +0800

    Site updated: 2021-03-04 20:03:14

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    mini: true,
+    audio: [{
+        name: 'Once Again',
+        artist: 'Tristam',
+        url: '/disc/Tristam - Once Again.flac',
+        cover: '/disc/Tristam - Once Again.jpeg',
+    }]
+});
