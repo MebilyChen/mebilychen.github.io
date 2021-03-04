c0380b2fee876fbcb3a2e19dbea2c9fc8eeb179c
mebilychen <1013712127@qq.com>
2021-03-04 20:32:40 +0800

    Site updated: 2021-03-04 20:32:39

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,15 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('aplayer'),
     mini: true,
+    theme: '#FADFA3', //主题色
     autoplay: false, //自动播放
     listFolded: true,
+    loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
     audio: [{
         name: 'Once Again',
         artist: 'Tristam',
-        url: '/disc/Tristam - Once Again.flac',
-        cover: '/disc/Tristam - Once Again.jpeg',
+        url: '/dist/Tristam - Once Again.flac',
+        cover: '/dist/Tristam - Once Again.jpeg',
 
         //name: 'name2', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
         //artist: 'artist2',
