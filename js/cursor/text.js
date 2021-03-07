db5d3023c6c41c7ccd6b5fc2acb644f07ca41d65
mebilychen <1013712127@qq.com>
2021-03-07 21:06:26 +0800

    Site updated: 2021-03-07 21:06:25

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -5,7 +5,7 @@ if(i==0){
   var a = new Array("陛下万岁！");
 }
   if(i==1){
-  var a = new Array("🤍","🖤","🤍");
+  var a = new Array("🖤","🤍","🖤","陛下难道🤍不可爱吗","🖤","🤍","🖤");
   }
   if(i==2){
       var a = new Array("一定要把@memo完结掉！");
