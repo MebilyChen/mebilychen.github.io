e765d849b904e01439251e3fd4a9ccee4b4924ff
mebilychen <1013712127@qq.com>
2021-03-07 16:56:33 +0800

    Site updated: 2021-03-07 16:56:32

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -3,8 +3,7 @@ jQuery(document).ready(function($) {
   $("body").click(function(e) {
     var a = new Array("陛下万岁！", "今天也要开心呀", "加油","🤍","🖤","一定要把@memo完结掉！");
     var $i = $("<span/>").text(a[a_idx]);
-    var x = e.pageX,
-    y = e.pageY;
+    var x = e.pageX,y = e.pageY;
     $i.css({
       "z-index": 99999,
       "top": y - 28,
@@ -16,7 +15,7 @@ jQuery(document).ready(function($) {
     $i.animate({
       "top": y - 180,
       "opacity": 0
-    }, 1500, function() {
+    }, 1000, function() {
       $i.remove();
     });
     a_idx = (a_idx + 1) % a.length;
