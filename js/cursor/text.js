5951cb24cafd7fee3208104e2893b9eac5925f51
mebilychen <1013712127@qq.com>
2021-03-07 17:22:49 +0800

    Site updated: 2021-03-07 17:22:48

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,7 +1,8 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
   $("body").click(function(e) {
-    var a = new Array("陛下万岁！", "今天也要开心呀", "加油","🤍","🖤","一定要把@memo完结掉！");
+    //var a = new Array("陛下万岁！", "今天也要开心呀", "加油","🤍","🖤","一定要把@memo完结掉！");
+    var a = new Array("1", "22", "333","4444","55555","666666");
     var $i = $("<span/>").text(a[a_idx]);
     var x = e.pageX,y = e.pageY;
     $i.css({
