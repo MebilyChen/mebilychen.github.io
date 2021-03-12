ad3d599c57aa5852b635d131ca5a1889c5772349
碳素 <71856534+MebilyChen@users.noreply.github.com>
2021-03-12 13:50:58 +0800

    Site updated: 2021-03-12 13:50:56

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -16,7 +16,7 @@
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
          TagCanvas.textColour = '#202124';
-         TagCanvas.textHeight = 5;
+         TagCanvas.textHeight = 10;
          TagCanvas.outlineColour = '#E2E1D1';
          TagCanvas.maxSpeed = 0.2;
          TagCanvas.freezeActive = false;
